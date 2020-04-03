0f6100ce96048e274c8a9e61af3fca3a9256b514
Jeff Sutch <jeff.sutch@acm.org>
2020-04-03 12:29:52 -0700

    breaking out items into a new file and storing items in sqlite

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_jwt import JWT, jwt_required, current_identity
 # our libraries
 from security import authenticate, identity # implemented in security.py
 from user import UserRegister
+from item import Item, Itemlist
 
 # instantiate app
 app = Flask(__name__)
@@ -17,7 +18,7 @@ api = Api(app)
 jwt = JWT(app, authenticate, identity)
 
 # datastore
-items = []
+# items = []
 
 class Test(Resource):
     """
@@ -26,67 +27,6 @@ class Test(Resource):
     def get(self, name):
         return {'test':name}
 
-class Item(Resource):
-    """
-    Main Item Class
-    Flask-RESTful does not need jsonify for returns
-    """
-    # Move parser within item instead of function by function
-    parser = reqparse.RequestParser() #reqparser can also be used for form fields, also
-    parser.add_argument('price',
-        type=float,
-        required=True,
-        help='This field cannot be left blank'
-    )
-    #@jwt_required()
-    def get(self, name):
-        item = next(filter(lambda x: x['name'] == name, items), None) #next() returns only the first item from filter(lambda)
-        # return {'item': item}, 200 if item is not None else 404
-        return {'item': item}, 200 if item else 404 #shortened version
-
-    #@jwt_required()
-    def post(self, name):
-        if next(filter(lambda x: x['name'] == name, items), None) is not None:
-            return {'message': "An item with name '{}' already exists".format(name)}, 400
-
-        data = Item.parser.parse_args()
-
-        item = {
-            'name' : name,
-            'price' : data['price']
-            }
-        items.append(item)
-        return item, 201
-
-    #@jwt_required()   
-    def delete(self, name):
-        """
-        Overwrite items list with a new list that has had 'name' removed
-        """
-        global items 
-        items = list(filter(lambda x: x['name'] != name, items))
-        return {'message':'Item Deleted'}
-
-    #@jwt_required()
-    def put(self, name):
-        """
-        Itempotent. Can create or update an item.
-        """
-        data = Item.parser.parse_args()
-
-        item = next(filter(lambda x: x['name'] == name, items), None)
-        if item is None:
-            item = {'name':name,'price': data['price']}
-            items.append(item)
-        else:
-            item.update(data)
-        return item 
-
-
-class ItemList(Resource):
-    @jwt_required()
-    def get(self):
-        return {'items': items}
 
 # Adding resources:
 # api.add_resource(xxx) replaces @app.route('xxx') under Student:get   
